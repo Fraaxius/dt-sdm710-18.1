ef326400921d2773b996425fbc37b75205390f72
Fraaxius <franthekillaz@gmail.com>
2024-09-07 23:13:41 -0400

    sdm710-common: AICP-ify

--- common.mk
+++ common.mk
@@ -30,12 +30,12 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-aicp/lineage-sdk \
+    $(LOCAL_PATH)/overlay-aicp/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true
